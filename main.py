416520344a205c9941d140ef7b0564774f01d796
TalanAria <taria@cocc.edu>
2025-01-06 11:57:03 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ pet_type = "Cat"
 pet_name = "bob"
 print(f"I have a {pet_type} and his name is {pet_name}.")
 
+
+
+
+
+
+
 name = input("Enter your first name:")
 age = int(input("Enter your current age:"))
 savings = int(input("Enter your yearly savings:"))
 
 print(f"Hello {name}! You are currently {age} years old. In 10 years, you will be {age+10} years old. If you save ${savings} each year, in 5 years you will have saved ${savings*5}.")
 print(f"Your average monthly savings is ${savings/12}")
+
+
+
+
+
+
+
+
+total_eggs = int(input("Enter the number of eggs:"))
+
+print(f"This makes {total_eggs//12} dozen eggs with {total_eggs%12} left over")
